1a014f79c2709e42850a7304e3c6ececdd3c4f98
Gattem Phanindra <20pa1a0547@vishnu.edu.in>
2024-08-31 21:18:16 +0530

    intial commit

---
+++ video4/scrpit.js
@@ -0,0 +1,20 @@
+// for arrayu we us square brackets
+//for object we use curly brackets - used for key pair values
+
+let att =["object","class","oops",{class:"blueprint"},"7"]
+console.log(att)
+console.log(typeof att)//every thing in js is a object
+console.log(Array.isArray(att))//checking it is array or not
+
+
+
+const jd={
+    class:"blueprint",
+    object:"instance",
+    abstraction:"unwanted",
+    encapsulation:"binding",
+    polymorphism:"manyforms",
+    inheritance:"DNA"
+}
+console.log(jd)
+console.log(typeof jd)
