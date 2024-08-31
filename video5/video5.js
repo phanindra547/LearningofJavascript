3ba3fdbee760e96675821cc5a8a6f868095c1ca6
Gattem Phanindra <20pa1a0547@vishnu.edu.in>
2024-08-31 22:46:52 +0530

    world

---
+++ video5/video5.js
@@ -0,0 +1,13 @@
+//conditions and control flow statements
+var code="java";
+let platform="vscode";
+const jonoffers=0;
+if(code=="javga"){
+    document.write("I am using java as a default language")
+
+}else if(platform=="vscojde"){
+    document.write("Currently working on the frontend projects")
+}else{
+    document.write("there are no job offers for me")
+}
+
---
+++ video7/dialog.js
@@ -0,0 +1,14 @@
+//Dialog Boxes its like a quick message appear on web site
+//alert
+//alert("This is very unbarble situation")
+
+//prompt("How many projets you have done")
+
+//confirm("Are you confident to get a job")
+
+var se = confirm("are you women")
+if(se){
+    document.write("Heartly welcome "+prompt("Enter your Name")+" to my world")
+}else{
+    document.write("Heartly welcome bro "+prompt("Enter your Name")+" lets go for my webpage")
+}
